9ac2ddfe846fd76f6accfd92e1121cf02a56a063
Dave Pike <pike.dt@gmail.com>
2019-03-31 16:44:26 -0400

    Added install scripts originally in dotfiles

---
+++ install_dev_essentials.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+  autoconf \
+  build-essential \
+  ccache \
+  cifs-utils \
+  clang-format \
+  clang-tidy \
+  cmake \
+  cppcheck \
+  distcc \
+  doxygen \
+  gdb \
+  gdb \
+  git \
+  libclang-dev \
+  libpython-dev \
+  libpython3-dev \
+  libtool \
+  make \
+  ninja-build \
+  ninja-build \
+  openssh-server \
+  python-defusedxml \
+  python-netifaces \
+  python-pip \
+  python-setuptools \
+  rsync \
+  silversearcher-ag \
+  software-properties-common \
+  tmux \
+  tree \
+  xsel
+
+

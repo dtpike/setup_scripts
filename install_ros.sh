4bf7b86182fa273289aa2bda4b582e8e3f12e0ec
Dave Pike <pike.dt@gmail.com>
2019-11-22 14:02:48 +0000

    Fixed install_ros.sh script

--- install_ros.sh
+++ install_ros.sh
@@ -8,9 +8,9 @@ fi
 VERSION=$1
 if [ ! -f /etc/apt/sources.list.d/ros-latest.list ]; then
     sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 fi
 
 apt-get update
-apt-get install ros-$VERSION-ros-base
+apt-get install -y ros-$VERSION-ros-base
 

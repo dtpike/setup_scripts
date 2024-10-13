6ca645020f68450d21374e3f7326c5002636ab88
Dave Pike <david.pike@ouster.io>
2024-10-13 09:35:29 -0400

    Update script to install llvm

--- install_llvm.sh
+++ install_llvm.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+set -e
+
 wget https://apt.llvm.org/llvm.sh -P /tmp
 chmod +x /tmp/llvm.sh
-VERSION=15
-/tmp/llvm.sh $VERSION
+VERSION=13
+sudo /tmp/llvm.sh $VERSION
 sudo apt install -y clang-format-$VERSION clang-tidy-$VERSION libc++-$VERSION-dev libc++abi-$VERSION-dev
+
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-$VERSION $VERSION

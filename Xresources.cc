34add3eb87dcc95a6ded3156751483a836a8ad3b
Dave Pike <david.pike@ouster.io>
2024-10-13 09:37:45 -0400

    Remove unsed files

--- Xresources.cc
+++
@@ -1,9 +0,0 @@
-! urxvt config
-URxvt.scrollBar: false
-! This font info can be found from the command fc-list:
-URxvt.font:     xft:Meslo LG M for Powerline:style=RegularForPowerline:size=12:antialias=true
-URxvt.boldFont: xft:Meslo LG M for Powerline:style=RegularForPowerline:bold:size=12:antialias=true
-! Remove border
-URxvt.internalBorder: 0
-URxvt.letterSpace: -1
-
